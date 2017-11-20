a5e716aa9086517556e259d15feda62f9e6e2ce1
Daksh Jotwani <dakshjay@gmail.com>
2017-11-20 16:08:27 -0500

    Print progress of fetching sentences, Change file writing mode to 'append'

--- get_sentences_from_wiki.py
+++ get_sentences_from_wiki.py
@@ -6,7 +6,7 @@ from wikipedia.exceptions import DisambiguationError, PageError
 
 
 def get_sentences_from_wiki(sentences_amount=100, print_sentence=False,
-                            save_to_file=False, file_name=None):
+                            save_to_file=False, file_name=None, print_progress=False):
     # Working on the assumption that one page will give at least one sentence averagely
     titles = w.random(sentences_amount)
 
@@ -27,6 +27,11 @@ def get_sentences_from_wiki(sentences_amount=100, print_sentence=False,
                     print(sentence)
 
                 count += 1
+
+                if print_progress:
+                    percentage = count * 100 / sentences_amount
+                    if (percentage % 10) == 0:
+                        print('%s%% (%s / %s)' % (percentage, count, sentences_amount))
 
                 if count == sentences_amount:
 
@@ -63,12 +68,13 @@ def skip_sentence(sentence, min_words=10):
 
 
 def save_sentences_to_file(lines, file_name):
-    file = open(file_name, 'w')
+    file = open(file_name, 'a')
 
     for x in lines:
         file.write('%s\n' % x)
 
 
 if __name__ == '__main__':
-    sentences = get_sentences_from_wiki(sentences_amount=10000, print_sentence=False,
-                                        save_to_file=True, file_name='sentences_from_wiki.txt')
+    sentences = get_sentences_from_wiki(sentences_amount=1000, print_sentence=False,
+                                        save_to_file=True, file_name='sentences_from_wiki.txt',
+                                        print_progress=True)
