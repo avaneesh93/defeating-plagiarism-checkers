759085471aa8f10ad41b5a38e7c376868aa60d8b
Avaneesh Reddy Gavva <agavva@umass.edu>
2017-11-19 20:03:15 -0500

    WSD synsets

--- get_synonym_temp.py
+++ get_synonym_temp.py
@@ -1,4 +1,21 @@
 from nltk.corpus import wordnet as wn
+from nltk.wsd import lesk
+
+
+def get_sysnset():
+    sent1 = ['He', 'is', 'going', 'to', 'stand', 'for', 'office', '.']
+    sent2 = ['He', 'is', 'going', 'to', 'the', 'bicycle', 'stand', '.']
+
+    ss1 = lesk(sent1, 'stand')
+    ss2 = lesk(sent2, 'stand')
+
+    print(ss1)
+    print(ss1.definition())
+    print(ss1.lemma_names())
+
+    print(ss2)
+    print(ss2.definition())
+    print(ss2.lemma_names())
 
 
 def get_synonym(word, pos):
@@ -10,4 +27,5 @@ def get_synonym(word, pos):
 
 
 if __name__ == '__main__':
-    get_synonym('smart', wn.ADJ)
+    # get_synonym('smart', wn.ADJ)
+    get_sysnset()
