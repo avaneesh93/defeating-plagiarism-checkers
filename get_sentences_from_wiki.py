3a592be2616eda69c722374a811184a522a50192
Daksh Jotwani <dakshjay@gmail.com>
2017-11-20 15:43:37 -0500

    Add various sentence filtering conditions

--- get_sentences_from_wiki.py
+++ get_sentences_from_wiki.py
@@ -5,7 +5,7 @@ from nltk import sent_tokenize
 from wikipedia.exceptions import DisambiguationError
 
 
-def get_sentences_from_wiki(wiki_pages=1):
+def get_sentences_from_wiki(wiki_pages=1, print_sentence=False):
     titles = w.random(wiki_pages)
 
     result = []
@@ -14,12 +14,13 @@ def get_sentences_from_wiki(wiki_pages=1):
         try:
             page = w.page(title=title)
 
-            for sentence in sent_tokenize(page.summary):
-                # Skip sentences with numbers in them
-                if re.search(r'\d', sentence):
+            for sentence in sent_tokenize(page.content):
+                if skip_sentence(sentence):
                     continue
 
                 result.append(sentence)
+                if print_sentence:
+                    print(sentence)
 
         except DisambiguationError:
             pass
@@ -27,6 +28,24 @@ def get_sentences_from_wiki(wiki_pages=1):
     return result
 
 
+def skip_sentence(sentence, min_words=10):
+    # Skip sentences with less than the specified amount of words
+    if len(sentence.split()) < min_words:
+        return True
+
+    # Skip sentences that DON'T start with a capital letter and DON'T end with a period
+    if not re.search(r'(?<=^[A-Z]).+(?=\.$)', sentence):
+        return True
+
+    # Skip sentences with numbers in them
+    if re.search(r'\d', sentence):
+        return True
+
+    # Skip sentences with capital letters in them (probably proper nouns),
+    #  except the first letter.
+    if re.search(r'[A-Z]', sentence[1:]):
+        return True
+
+
 if __name__ == '__main__':
-    for s in get_sentences_from_wiki(wiki_pages=10):
-        print(s)
+    get_sentences_from_wiki(wiki_pages=100, print_sentence=True)
