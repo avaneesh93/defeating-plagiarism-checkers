cd7ecba5243e7bbb19b74a5e3cae01413d1fdc4e
Daksh Jotwani <dakshjay@gmail.com>
2017-12-22 12:24:01 -0500

    Add NLTK download statement for averaged_perceptron_tagger

--- src/pos.py
+++ src/pos.py
@@ -2,6 +2,8 @@ import nltk
 
 
 def set_parts_of_speech_in_tokens(all_tokens_of_all_sentences):
+    nltk.download('averaged_perceptron_tagger')
+
     for sentence in all_tokens_of_all_sentences:
         for token in sentence:
             token.pos = nltk.pos_tag([token.word_without_punctuations])[0]
