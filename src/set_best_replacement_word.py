a734c80762294257b6105bd7773770c1c8b85d56
Daksh Jotwani <dakshjay@gmail.com>
2017-12-22 17:29:14 -0500

    Add None checks to word replacements

--- src/set_best_replacement_word.py
+++ src/set_best_replacement_word.py
@@ -7,8 +7,13 @@ def set_best_replacement_word(all_tokens_of_all_sentences):
             if not is_candidate_type(token.pos):
                 continue
 
-            for lang_rep, _ in token.replacements_langmod_and_prob:
-                for logreg_rep in token.replacements_logreg:
-                    if lang_rep == logreg_rep:
+            if token.replacements_langmod_and_prob:
+                for lang_rep, _ in token.replacements_langmod_and_prob:
+                    if token.replacements_logreg:
+                        for logreg_rep in token.replacements_logreg:
+                            if lang_rep == logreg_rep:
+                                token.replaced_word = lang_rep
+                                break
+                    else:
                         token.replaced_word = lang_rep
                         break
