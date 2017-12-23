e7cffca79e2192e77c72ab7dd026712fd28057ac
Daksh Jotwani <dakshjay@gmail.com>
2017-12-22 19:37:36 -0500

    Fix POS saving to save only the POS

--- src/pos.py
+++ src/pos.py
@@ -6,4 +6,4 @@ def set_parts_of_speech_in_tokens(all_tokens_of_all_sentences):
 
     for sentence in all_tokens_of_all_sentences:
         for token in sentence:
-            token.pos = nltk.pos_tag([token.word_without_punctuations])[0]
+            _, token.pos = nltk.pos_tag([token.word_without_punctuations])[0]
